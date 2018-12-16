6b63c6e3794d95e28ffbf4fe62c77ecaf9f56ed5
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-16 19:01:03 +0700

    Create helpop.js

---
+++ commands/network/helpop.js
@@ -0,0 +1,47 @@
+const Discord = require("discord.js");
+const commando = require('discord.js-commando')
+const prefix = "/";
+const bot = new commando.Client({
+  commandPrefix: prefix
+});
+const ms = require("ms");
+
+class helpopCommand extends commando.Command {
+  constructor(client) 
+  {
+    super(client, {
+      name: 'helpop', 
+      group: 'network',
+      memberName: 'helpop',
+      description: "Provides a user's request"
+    });
+  }
+
+  async run(message,args)
+  {
+    let requestargs = message.content.slice(prefix.length).split(/ + /); //MAIN ARGS
+        let requestmessage = requestargs.join(" ").slice(7);
+        if (!requestmessage) return message.channel.send({embed: new Discord.RichEmbed()
+          .setDescription(":x: **Missing args**")
+          .setColor("#FF4040")
+          .addField("->", ">helpop [request]")});
+
+            var requestembed = new Discord.RichEmbed()
+            .setDescription('**Request | Information**')
+            .setColor("#4286f4")
+            .addField('**Request By**', `${message.author} with the ID: ${message.author.id}`)
+            .addField('**User Request**', requestmessage)
+            .addField('**Request Location**', message.channel)
+            .addField('**Request Time**', message.createdAt)
+
+            let logschannel = message.guild.channels.find(`name`, "logs");
+            if(!logschannel) return message.channel.send("Couldn't find the logs channel");
+
+            message.delete(3000);
+            logschannel.send(requestembed);
+
+        message.reply('Thanks for your request. We will contact you within 24h')
+  }
+}
+
+module.exports = helpopCommand
