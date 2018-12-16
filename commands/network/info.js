027990f7271c7a7123f2f8cbf9231378964cde4f
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-16 21:19:32 +0700

    Create info.js

---
+++ commands/network/info.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const commando = require('discord.js-commando')
+const prefix = ">";
+const bot = new commando.Client({
+  commandPrefix: prefix
+});
+const ms = require("ms");
+
+class infoCommand extends commando.Command {
+  constructor(client) 
+  {
+    super(client, {
+      name: 'info', 
+      group: 'network',
+      memberName: 'info',
+      description: "Information about the Zilex Network"
+    });
+  }
+
+  async run(message)
+  {
+      message.channel.send({embed: new Discord.RichEmbed()
+        .setTitle("**Information**")
+        .setColor("#4286f4")
+        .addField("__Twitter__", "@USZilex")
+        .addField("__Website__", "www.zilex.us")
+        .addField("__Teamspeak__", "ts.zilex.us")
+        .addField("__Shop__", "shop.zilex.us")
+        .setFooter("Copyright© 2018 Zilex Network")
+        .setThumbnail("https://images-ext-1.discordapp.net/external/NKKFTtifMQP0ADT4Dw0BZd5fOEMHyd6NmrRznNTzm04/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/521027621589483542/db986c0e576e96cfe02c20ed7d814a69.png?width=487&height=487")})
+  }
+}
+
+module.exports = infoCommand;
