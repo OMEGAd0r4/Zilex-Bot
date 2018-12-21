838982d73d9dceb7671938272063d2640a4abf6c
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-21 18:19:50 +0700

    Update info.js

--- commands/network/info.js
+++ commands/network/info.js
@@ -25,7 +25,7 @@ class infoCommand extends commando.Command {
         .addField("__Twitter__", "@USZilex")
         .addField("__Website__", "www.zilex.us")
         .addField("__Teamspeak__", "ts.zilex.us")
-        .addField("__Shop__", "shop.zilex.us")
+        .addField("__Shop__", "zilex.buycraft.net")
         .setFooter("Zilex Network | Created by dora#0843")
         .setThumbnail("https://images-ext-1.discordapp.net/external/NKKFTtifMQP0ADT4Dw0BZd5fOEMHyd6NmrRznNTzm04/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/521027621589483542/db986c0e576e96cfe02c20ed7d814a69.png?width=487&height=487")})
   }
