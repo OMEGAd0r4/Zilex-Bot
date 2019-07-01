ed46669cb48e34c692e7a2b9262c90bbe12bcd26
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2019-07-01 16:30:28 +0700

    Update ip.js

--- commands/network/ip.js
+++ commands/network/ip.js
@@ -20,11 +20,10 @@ class ipCommand extends commando.Command {
     {
         var ipEmbed = new Discord.RichEmbed()
             .setColor("#4286f4")
-            .setTitle("**Zilex | Information**")
-            .addField(":mailbox_with_no_mail: SA: Server IP", "-> **sa.zilex.us**")
-            .addField(":mailbox_with_no_mail: EU: Server IP", "-> **eu.zilex.us**")
+            .setTitle("**Zelix | Information**")
+            .addField(":mailbox_with_no_mail: Server IP", "-> **eu.zelix.us**")
             .addField("``>IP to use this command. Requested by``", ` -> ${message.author}`)
-            .setFooter("Copyright© 2018 Zilex Network. Created by dora#0843")
+            .setFooter("Copyright© 2018 Zelix Network. Created by dora#0843")
             .setThumbnail("https://images-ext-1.discordapp.net/external/NKKFTtifMQP0ADT4Dw0BZd5fOEMHyd6NmrRznNTzm04/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/521027621589483542/db986c0e576e96cfe02c20ed7d814a69.png?width=487&height=487")
 
         message.channel.send(ipEmbed);
